850fea7200f9dd5f23dd1ea8645b7122fdce4f25
Pranjal-Malik <pranjalsmalik@gmail.com>
2022-03-26 20:04:37 +0800

    Added Default Room, and 1 treasure room

---
+++ Unity Project/Ninja-Frog/Assets/Scripts/CameraTest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour
+{
+    private Vector3 StartingRoom = new Vector3(0,0,-10);
+    private Vector3 TreasureRoomRight = new Vector3(30,0,-10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = StartingRoom;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Switches Cameras Through Rooms.
+        if (Input.GetKeyDown("1"))
+        {
+            transform.position = StartingRoom;
+
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+
+            transform.position = TreasureRoomRight;
+        }
+
+
+    }
+}
